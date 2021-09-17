64f5ed508e13636ec461f63f3f7631ce08d4cfda
datn02 <ducanh02.work@gmail.com>
2021-09-17 17:16:08 +0800

    Add new codes

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,13 +16,16 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+        //System.out.println("Printing deadlines");
+        //printDeadlines(tasksData);
+
+        //System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        //printData(tasksData);
+        //printDataWithStreams(tasksData);
         printDeadlineUsingStream(tasksData);
         System.out.println("Total number of deadlines: " +
                 countDeadlinesUsingStream(tasksData));
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
 
@@ -36,10 +39,25 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data by looping");
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream");
+        tasks.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
